4c31f83677307471fe30f22a47b69348182a0e53
temoty <temoty@hotmail.com>
2019-09-09 01:09:14 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class FetchAUser extends Component {
+  
+
+  state = {
+    loading: true,
+    person: null
+  }
+
+  async componentDidMount() {
+  
+
+    const url = 'https://randomuser.me/api/';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.results[0], loading: false });
+
+
+  }
+
+  render() {
+
+  
+
+    return (
+    <div className="App">
+      { this.state.loading || !this.state.person ? (
+          <div>loading...</div>
+        ) : (
+          <div>
+            <div>Random Person from API...</div>
+            <div>Title: { this.state.person.name.title }</div>
+            <div>First: { this.state.person.name.first }</div>
+            <div>Last: { this.state.person.name.last }</div>
+            <img src={ this.state.person.picture.large } />
+          </div>
+          )}
+    </div>
+    );
+  }
+}
+
+export default FetchAUser;
